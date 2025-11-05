52964f01caa26dc13a7fc65c96e26f2245a6a430
EKL-NCI <x22353461@student.ncirl.ie>
2025-11-05 21:03:51 +0000

    🐛 Fixes to correct errors on CI/CL

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,7 +1,8 @@
 module AlbumsHelper
-    def star_rating(rating)
+  def star_rating(rating)
+    rating = rating.to_i.clamp(0, 5)
     full_stars = "&#9733;" * rating
     empty_stars = "&#9734;" * (5 - rating)
-    full_stars + empty_stars
+    (full_stars + empty_stars).html_safe
   end
 end

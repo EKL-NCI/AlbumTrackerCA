6546760853fed18da8cee3f1670fa0b8eb4ef3a0
EKL-NCI <x22353461@student.ncirl.ie>
2025-12-16 02:17:16 +0000

    💄Fix connect to frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:5000" # Flask frontend
+    origins "http://localhost:5000", "http://127.0.0.1:5000" # Flask frontend
     resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options ]
   end
 end
